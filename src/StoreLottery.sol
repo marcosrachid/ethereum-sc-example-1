8bea15c84fe879472500b33c87d545dad945ef05
Marcos Rachid <marcosrachid@gmail.com>
2018-11-01 17:34:25 -0300

    modifier and change implementation

--- src/StoreLottery.sol
+++ src/StoreLottery.sol
@@ -8,7 +8,7 @@ contract StoreLottery {
     uint lotteryCounter = 0;
 
     constructor(uint startNumber) public {
-        require(msg.sender.balance > 99.999999999999 ether);
+        require(msg.sender.balance > 99.999999 ether);
 
         luckyNumber = startNumber;
         owner = msg.sender;
@@ -20,9 +20,22 @@ contract StoreLottery {
         }
     }
 
-    function set(uint sent) public {
+    modifier minimunCost(uint min) {
+      require(msg.value >= min, "Insuficient Ether.");
+      _; // continue to run remaing function code
+    }
+
+    event changeEvent(address payer, uint change);
+
+    function set(uint sent) public payable minimunCost(1000) {
         luckyNumber = sent;
         lotteryCounter++;
+
+        if (msg.value > 1000) {
+          uint change = msg.value - 1000;
+          msg.sender.transfer(change);
+          emit changeEvent(msg.sender, change);
+        }
     }
 
     function get() public view returns (uint) {
